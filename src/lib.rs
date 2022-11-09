6cb027b8bc7409d65df4a383073f1c7900b6489a
JVCAv1 <joao.assumpo@gmail.com>
2022-11-08 23:05:53 -0300

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,30 @@
-
+use std::{fs::{OpenOptions}};
 
 pub fn insertion_sort(array: &mut Vec<usize>) -> Vec<usize> {
-	for i in 1..array.len() {
-		let mut j = i;
-		while j > 0 && array[j - 1] > array[j] {
-			array.swap(j - 1, j);
-			j -= 1;
-		}
-	}
-	array.to_vec()
+    for i in 1..array.len() {
+        let mut j = i;
+        while j > 0 && array[j - 1] > array[j] {
+            array.swap(j - 1, j);
+            j -= 1;
+        }
+    }
+    array.to_vec()
+}
+
+pub fn sort_file(file_path: &str){
+	let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(file_path);
+	unimplemented!("sort_file");
 }
 
 #[test]
 fn test_insertion_sort() {
-	let mut array = vec![5, 2, 4, 6, 1, 3];
-	println!("unsorted: {:?}", array);
-	let sorted_array = insertion_sort(&mut array);
-	println!("sorted: {:?}", sorted_array);
-	assert_eq!(sorted_array, vec![1, 2, 3, 4, 5, 6]);
+    let mut array = vec![5, 2, 4, 6, 1, 3];
+    println!("unsorted: {:?}", array);
+    let sorted_array = insertion_sort(&mut array);
+    println!("sorted: {:?}", sorted_array);
+    assert_eq!(sorted_array, vec![1, 2, 3, 4, 5, 6]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use insertion_sort_rs::insertion_sort;
+use std::env::args;
+use insertion_sort_rs::*;
 
 fn main(){
-	unimplemented!()
+	let args: Vec<String> = std::env::args().collect();
+	let mut a: Vec<i32> = Vec::new();
+
+	if args.len() > 2 {
+		for i in 0..args.len() {
+			if let Ok(parsed) = args[i].parse::<i32>(){
+				a.push(parsed);
+			} else {
+				panic!("Invalid argument! Integers are required.");
+			}
+		}
+	} else {
+		panic!("Not enough arguments! Must be bigger than 2.")
+	}
 }
