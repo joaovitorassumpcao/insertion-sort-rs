c6a6167dcf9812f30c4995f6e72882e7e23d09f2
JVCAv1 <joao.assumpo@gmail.com>
2022-11-10 18:10:21 -0300

    Long help

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ fn main() {
                 .short('l')
                 .long("list")
                 .conflicts_with("file")
-                .help("Manually typed values separated by spaces, e.g 9 3 2 7 4 ")
+                .help("Manually typed values.")
+                .long_help(
+                    "Manually typed values. separated by spaces,\n".to_owned()
+                        + "e.g:   -l 9 3 2 7 4 3 21 2 491 2 1",
+                )
                 .action(ArgAction::Set)
                 .num_args(1..),
         )
@@ -21,8 +25,14 @@ fn main() {
                 .long("file-path")
                 .conflicts_with("list")
                 .help("Path to the file in which the contents will be sorted")
+                .long_help(
+                    "Path to the file in which the contents will be sorted,\n".to_owned()
+                        + "the values must be separated by spaces and be on the\n"
+                        + "last line of the file.\n"
+                        + "e.g:    -f /path/to/file.txt",
+                )
                 .action(ArgAction::Set)
-                .num_args(1)
+                .num_args(1),
         )
         .get_matches();
 
@@ -30,7 +40,7 @@ fn main() {
     if cmd.get_one::<String>("list").is_some() {
         let sorted_nums = arg_list(&cmd);
         print!("Sorted: ");
-        sorted_nums.iter().for_each(|x| print!(" {}", x));
+        sorted_nums.iter().for_each(|x| print!("{}", x));
         println!(" ");
     }
 
